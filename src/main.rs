ec7789052d008f48f987dfcdb9666319e20d3883
Bradley Wood <git@bradleywood.com>
2020-04-11 00:00:22 +0100

    fix ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//! [![pipeline status](https://gitlab.com/bradwood/git-lab-rust/badges/master/pipeline.svg)](https://gitlab.com/bradwood/git-lab-rust/-/commits/master)
+//! [![coverage report](https://gitlab.com/bradwood/git-lab-rust/badges/master/coverage.svg)](https://gitlab.com/bradwood/git-lab-rust/-/commits/master)
+//!
 //! This is a cli tool that adds the `lab` command to `git` to enable interaction with a GitLab server.
 //!
 //! # Features
