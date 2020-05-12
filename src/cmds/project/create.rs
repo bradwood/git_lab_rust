0c2e1ff4d61ae9452cb5c737faee4f93399d9525
Bradley Wood <git@bradleywood.com>
2020-05-12 23:55:25 +0100

    format code and linting

--- src/cmds/project/create.rs
+++ src/cmds/project/create.rs
@@ -6,10 +6,10 @@ use crate::gitlab::{
     visibility_level_from_str, CreateProjectParams, IfGitLabCreateProject,
 };
 
-    CreateProjectParams,
-    IfGitLabCreateProject};
-
-pub fn create_project_cmd(args: clap::ArgMatches, gitlab: impl IfGitLabCreateProject) -> Result<()> {
+pub fn create_project_cmd(
+    args: clap::ArgMatches,
+    gitlab: impl IfGitLabCreateProject,
+) -> Result<()> {
     let mut p = CreateProjectParams::builder();
 
     // basic casting to URL
@@ -100,7 +100,7 @@ pub fn create_project_cmd(args: clap::ArgMatches, gitlab: impl IfGitLabCreatePro
         p.auto_devops_deploy_strategy(args.value_of("auto_devops_deploy_strategy").unwrap());
     }
 
-    // specific cast to visibilily_level enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to visibilily_level enum - unwrap()'s are safe as problems will be caught by clap.rs
     if args.occurrences_of("visibility") > 0 {
         debug!("visibility");
         p.visibility(visibility_level_from_str(args.value_of("visibility").unwrap()).unwrap());
@@ -108,65 +108,94 @@ pub fn create_project_cmd(args: clap::ArgMatches, gitlab: impl IfGitLabCreatePro
 
     // FIXME these don't work for some reason -- try doing another post after the create to set
     // visibility requirements
-    // specific cast to feature_visibilily_level enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to feature_visibilily_level enum - unwrap()'s are safe as problems will be caught by clap.rs
     if args.occurrences_of("repo_access_level") > 0 {
         debug!("repo_access_level");
-        p.repository_access_level(feature_visibility_level_from_str(args.value_of("repo_access_level").unwrap()).unwrap());
+        p.repository_access_level(
+            feature_visibility_level_from_str(args.value_of("repo_access_level").unwrap()).unwrap(),
+        );
     }
     if args.occurrences_of("issues_access_level") > 0 {
         debug!("issues_access_level");
-        p.issues_access_level(feature_visibility_level_from_str(args.value_of("issues_access_level").unwrap()).unwrap());
+        p.issues_access_level(
+            feature_visibility_level_from_str(args.value_of("issues_access_level").unwrap())
+                .unwrap(),
+        );
     }
     if args.occurrences_of("mr_access_level") > 0 {
         debug!("mr_access_level");
-        p.merge_requests_access_level(feature_visibility_level_from_str(args.value_of("mr_access_level").unwrap()).unwrap());
+        p.merge_requests_access_level(
+            feature_visibility_level_from_str(args.value_of("mr_access_level").unwrap()).unwrap(),
+        );
     }
     if args.occurrences_of("builds_access_level") > 0 {
         debug!("builds_access_level");
-        p.builds_access_level(feature_visibility_level_from_str(args.value_of("builds_access_level").unwrap()).unwrap());
+        p.builds_access_level(
+            feature_visibility_level_from_str(args.value_of("builds_access_level").unwrap())
+                .unwrap(),
+        );
     }
     if args.occurrences_of("wiki_access_level") > 0 {
         debug!("wiki_access_level");
-        p.wiki_access_level(feature_visibility_level_from_str(args.value_of("wiki_access_level").unwrap()).unwrap());
+        p.wiki_access_level(
+            feature_visibility_level_from_str(args.value_of("wiki_access_level").unwrap()).unwrap(),
+        );
     }
     if args.occurrences_of("snippets_access_level") > 0 {
         debug!("snippets_access_level");
-        p.snippets_access_level(feature_visibility_level_from_str(args.value_of("snippets_access_level").unwrap()).unwrap());
+        p.snippets_access_level(
+            feature_visibility_level_from_str(args.value_of("snippets_access_level").unwrap())
+                .unwrap(),
+        );
     }
     if args.occurrences_of("pages_access_level") > 0 {
         debug!("pages_access_level");
-        p.pages_access_level(feature_visibility_level_from_str(args.value_of("pages_access_level").unwrap()).unwrap());
+        p.pages_access_level(
+            feature_visibility_level_from_str(args.value_of("pages_access_level").unwrap())
+                .unwrap(),
+        );
     }
 
-    // specific cast to merge_method enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to merge_method enum - unwrap()'s are safe as problems will be caught by clap.rs
     if args.occurrences_of("merge_method") > 0 {
         debug!("merge_method");
         p.merge_method(merge_method_from_str(args.value_of("merge_method").unwrap()).unwrap());
     }
 
-    // specific cast to build_git_strategy enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to build_git_strategy enum - unwrap()'s are safe as problems will be caught by clap.rs
     if args.occurrences_of("pipeline_git_strategy") > 0 {
         debug!("pipeline_git_strategy");
-        p.build_git_strategy(pipeline_git_strategy_from_str(args.value_of("pipeline_git_strategy").unwrap()).unwrap());
+        p.build_git_strategy(
+            pipeline_git_strategy_from_str(args.value_of("pipeline_git_strategy").unwrap())
+                .unwrap(),
+        );
     }
 
     // FIXME array passing doesn't seem to work here... Investigate!
-    // cast to a Vec of Strings
+    // Upstream BUG: https://gitlab.kitware.com/utils/rust-gitlab/-/issues/34
     if args.occurrences_of("tag_list") > 0 {
         debug!("tag_list");
         p.tag_list(values_t_or_exit!(args, "tag_list", String));
     }
     if args.occurrences_of("container_expiration_policy") > 0 {
         debug!("container_expiration_policy");
-        p.container_expiration_policy_attributes(values_t_or_exit!(args, "container_expiration_policy", String));
+        p.container_expiration_policy_attributes(values_t_or_exit!(
+            args,
+            "container_expiration_policy",
+            String
+        ));
     }
 
     let params = p.build();
     debug!("params: {:#?}", params);
-
+    debug!("args: {:#?}", args);
     // TODO: Consider changing return value to Result<serde_json::Value> to get raw json.
     let project = gitlab
-        .create_project(args.value_of("name").unwrap(), args.value_of("path").or_else(|| None), Some(params.unwrap()))
+        .create_project(
+            args.value_of("name").unwrap(),
+            args.value_of("path").or_else(|| None),
+            Some(params.unwrap()),
+        )
         .context("Failed to create project - check for name or path clashes on the server")?;
 
     println!("Project id: {}", project.id);
