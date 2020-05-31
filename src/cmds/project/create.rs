8d4fc389a555d7ff00df98cc71c5967276818411
Bradley Wood <git@bradleywood.com>
2020-05-31 16:27:44 +0100

    add more options to create project

--- src/cmds/project/create.rs
+++ src/cmds/project/create.rs
@@ -37,6 +37,9 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
     if args.occurrences_of("namespace_id") > 0 {
         p.namespace_id(value_t_or_exit!(args, "namespace_id", u64));
     }
+    if args.occurrences_of("merge_approval_count") > 0 {
+        p.approvals_before_merge(value_t_or_exit!(args, "merge_approval_count", u64));
+    }
     if args.occurrences_of("build_timeout") > 0 {
         p.build_timeout(value_t_or_exit!(args, "build_timeout", u64));
     }
@@ -60,6 +63,9 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
     if args.occurrences_of("enable_shared_runners") > 0 {
         p.shared_runners_enabled(true);
     }
+    if args.occurrences_of("remove_source_branch_after_merge") > 0 {
+        p.remove_source_branch_after_merge(true);
+    }
     if args.occurrences_of("enable_public_builds") > 0 {
         p.public_builds(true);
     }
@@ -74,6 +80,21 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
     }
     if args.occurrences_of("auto_close_referenced_issues") > 0 {
         p.autoclose_referenced_issues(true);
+    }
+    if args.occurrences_of("disable_emails") > 0 {
+        p.emails_disabled(true);
+    }
+    if args.occurrences_of("enable_packages") > 0 {
+        p.packages_enabled(true);
+    }
+    if args.occurrences_of("enable_mirror") > 0 {
+        p.mirror(true);
+    }
+    if args.occurrences_of("mirror_triggers_builds") > 0 {
+        p.mirror_trigger_builds(true);
+    }
+    if args.occurrences_of("initialise_with_readme") > 0 {
+        p.initialize_with_readme(true);
     }
 
     // deprecated boolean disable flags
@@ -109,20 +130,19 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
 
     // EnableState enum from boolean
     if args.occurrences_of("auto_cancel_pending_pipelines") > 0 {
-        p.auto_cancel_pending_pipelines(enable_state_from_str(args.value_of("auto_cancel_pending_pipelines").unwrap()).unwrap());
+        p.auto_cancel_pending_pipelines(enable_state_from_str("enabled").unwrap());
     }
 
-    //  specific conversion to auto_devops_deploy_strategy enum - unwrap()'s are safe as problems will be caught by clap.rs
+    //  specific conversion to auto_devops_deploy_strategy enum
     if args.occurrences_of("auto_devops_deploy_strategy") > 0 {
         p.auto_devops_deploy_strategy(auto_devops_deploy_strategy_from_str(args.value_of("auto_devops_deploy_strategy").unwrap()).unwrap());
     }
-    //  specific conversion to visibilily_level enum - unwrap()'s are safe as problems will be caught by clap.rs
+    //  specific conversion to visibilily_level enum
     if args.occurrences_of("visibility") > 0 {
         p.visibility(visibility_level_from_str(args.value_of("visibility").unwrap()).unwrap());
     }
 
-    // specific conversion to feature_access_level enum - unwrap()'s are safe as problems will be
-    // caught by clap.rs
+    // specific conversion to feature_access_level enum
     // NOTE: API for these is currently buggy. See https://gitlab.com/gitlab-org/gitlab/-/issues/219482
     if args.occurrences_of("repo_access_level") > 0 {
         p.repository_access_level(
@@ -162,8 +182,7 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
             );
     }
 
-    // specific conversion to feature_access_level_public enum - unwrap()'s are safe as problems
-    // will be caught by clap.rs
+    // specific conversion to feature_access_level_public enum
     if args.occurrences_of("pages_access_level") > 0 {
         p.pages_access_level(
             feature_access_level_public_from_str(args.value_of("pages_access_level").unwrap())
@@ -171,12 +190,12 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
             );
     }
 
-    // specific conversion to merge_method enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to merge_method enum
     if args.occurrences_of("merge_method") > 0 {
         p.merge_method(merge_method_from_str(args.value_of("merge_method").unwrap()).unwrap());
     }
 
-    // specific conversion to build_git_strategy enum - unwrap()'s are safe as problems will be caught by clap.rs
+    // specific conversion to build_git_strategy enum
     if args.occurrences_of("pipeline_git_strategy") > 0 {
         p.build_git_strategy(
             pipeline_git_strategy_from_str(args.value_of("pipeline_git_strategy").unwrap())
@@ -185,7 +204,7 @@ pub fn generate_project_builder<'a>(args: &'a clap::ArgMatches, p: &'a mut Creat
     }
 
     if args.occurrences_of("tags") > 0 {
-        for t in values_t_or_exit!(args, "tag", String) {
+        for t in values_t_or_exit!(args, "tags", String) {
             p.tag(t);
         }
     }
@@ -217,18 +236,7 @@ pub fn create_project_cmd(
 
 #[cfg(test)]
 mod project_create_unit_tests {
-    // TODO: get mocking working....
-
-    // use std::fs::File;
-    // use std::io::BufReader;
-    // use std::path::Path;
-
-    // use anyhow::anyhow;
     use clap::SubCommand as ClapSubCommand;
-    // use gitlab::types::*;
-    // use serde::de::DeserializeOwned;
-
-    // use crate::gitlab::Project;
     use crate::subcommand::SubCommand;
     use crate::cmds::project;
 
@@ -247,8 +255,49 @@ mod project_create_unit_tests {
             .gen_clap_command()
             .get_matches_from(vec![
                 "project", "create", "project_name",
-                "-v", "private",
+                "--namespace_id", "3",
+                "--default_branch", "branch_name",
+                "-d", "description text",
+                "--issues_access_level", "disabled",
+                "--repo_access_level", "disabled",
+                "--forking_access_level", "disabled",
+                "--builds_access_level", "disabled",
+                "--mr_access_level", "disabled",
                 "--wiki_access_level", "disabled",
+                "--snippets_access_level", "disabled",
+                "--pages_access_level", "public",
+                "--disable_emails",
+                "--resolve_old_discussions",
+                "--enable_container_registry",
+                "--enable_shared_runners",
+                "-v", "private",
+                // "--import_url", "https://gitub.com/blah/blah.git", THIS NEEDS SEPARATE TESTING
+                "--enable_public_builds",
+                "--only_merge_on_good_ci",
+                "--only_merge_on_closed_discussions",
+                "--merge_method", "fast-forward",
+                "--auto_close_referenced_issues",
+                "--remove_source_branch_after_merge",
+                "--enable_lfs",
+                "--enable_request_access",
+                "--tags", "one,two",
+                "--print_merge_request_url",
+                "--pipeline_git_strategy", "clone",
+                "--build_timeout", "54",
+                "--auto_cancel_pending_pipelines",
+                "--build_coverage_regex", ".*",
+                "--ci_config_path", "filename",
+                "--enable_auto_devops",
+                "--auto_devops_deploy_strategy", "timed_incremental",
+                "--merge_approval_count", "3",
+                "--enable_mirror",
+                "--mirror_triggers_builds",
+                "--initialise_with_readme",
+                "--enable_packages",
+                "--disable_issues",
+                "--disable_mr",
+                "--disable_builds",
+                "--disable_snippets",
                 "--disable_wiki",
             ]);
         let matches = args.subcommand_matches("create");
@@ -257,124 +306,150 @@ mod project_create_unit_tests {
         let endpoint = generate_project_builder(&matches.unwrap(), &mut p);
 
         // THEN
-        let enpoint_debug = r###"CreateProject {
+        let endpoint_debug = r###"CreateProject {
     name_and_path: Name {
         name: "project_name",
     },
-    namespace_id: None,
-    default_branch: None,
-    description: None,
-    issues_access_level: None,
-    repository_access_level: None,
-    merge_requests_access_level: None,
-    forking_access_level: None,
-    builds_access_level: None,
+    namespace_id: Some(
+        3,
+    ),
+    default_branch: Some(
+        "branch_name",
+    ),
+    description: Some(
+        "description text",
+    ),
+    issues_access_level: Some(
+        Disabled,
+    ),
+    repository_access_level: Some(
+        Disabled,
+    ),
+    merge_requests_access_level: Some(
+        Disabled,
+    ),
+    forking_access_level: Some(
+        Disabled,
+    ),
+    builds_access_level: Some(
+        Disabled,
+    ),
     wiki_access_level: Some(
         Disabled,
     ),
-    snippets_access_level: None,
-    pages_access_level: None,
-    emails_disabled: None,
-    resolve_outdated_diff_discussions: None,
-    container_registry_enabled: None,
+    snippets_access_level: Some(
+        Disabled,
+    ),
+    pages_access_level: Some(
+        Public,
+    ),
+    emails_disabled: Some(
+        true,
+    ),
+    resolve_outdated_diff_discussions: Some(
+        true,
+    ),
+    container_registry_enabled: Some(
+        true,
+    ),
     container_expiration_policy_attributes: None,
-    shared_runners_enabled: None,
+    shared_runners_enabled: Some(
+        true,
+    ),
     visibility: Some(
         Private,
     ),
     import_url: None,
-    public_builds: None,
-    only_allow_merge_if_pipeline_succeeds: None,
-    only_allow_merge_if_all_discussions_are_resolved: None,
-    merge_method: None,
-    autoclose_referenced_issues: None,
-    remove_source_branch_after_merge: None,
-    lfs_enabled: None,
-    request_access_enabled: None,
-    tag_list: {},
-    printing_merge_request_link_enabled: None,
-    build_git_strategy: None,
-    build_timeout: None,
-    auto_cancel_pending_pipelines: None,
-    build_coverage_regex: None,
-    ci_config_path: None,
-    auto_devops_enabled: None,
-    auto_devops_deploy_strategy: None,
+    public_builds: Some(
+        true,
+    ),
+    only_allow_merge_if_pipeline_succeeds: Some(
+        true,
+    ),
+    only_allow_merge_if_all_discussions_are_resolved: Some(
+        true,
+    ),
+    merge_method: Some(
+        FastForward,
+    ),
+    autoclose_referenced_issues: Some(
+        true,
+    ),
+    remove_source_branch_after_merge: Some(
+        true,
+    ),
+    lfs_enabled: Some(
+        true,
+    ),
+    request_access_enabled: Some(
+        true,
+    ),
+    tag_list: {
+        "one",
+        "two",
+    },
+    printing_merge_request_link_enabled: Some(
+        true,
+    ),
+    build_git_strategy: Some(
+        Clone,
+    ),
+    build_timeout: Some(
+        54,
+    ),
+    auto_cancel_pending_pipelines: Some(
+        Enabled,
+    ),
+    build_coverage_regex: Some(
+        ".*",
+    ),
+    ci_config_path: Some(
+        "filename",
+    ),
+    auto_devops_enabled: Some(
+        true,
+    ),
+    auto_devops_deploy_strategy: Some(
+        TimedIncremental,
+    ),
     repository_storage: None,
-    approvals_before_merge: None,
+    approvals_before_merge: Some(
+        3,
+    ),
     external_authorization_classification_label: None,
-    mirror: None,
-    mirror_trigger_builds: None,
-    initialize_with_readme: None,
+    mirror: Some(
+        true,
+    ),
+    mirror_trigger_builds: Some(
+        true,
+    ),
+    initialize_with_readme: Some(
+        true,
+    ),
     template_name: None,
     template_project_id: None,
     use_custom_template: None,
     group_with_project_templates_id: None,
-    packages_enabled: None,
-    issues_enabled: None,
-    merge_requests_enabled: None,
-    jobs_enabled: None,
+    packages_enabled: Some(
+        true,
+    ),
+    issues_enabled: Some(
+        false,
+    ),
+    merge_requests_enabled: Some(
+        false,
+    ),
+    jobs_enabled: Some(
+        false,
+    ),
     wiki_enabled: Some(
         false,
     ),
-    snippets_enabled: None,
+    snippets_enabled: Some(
+        false,
+    ),
 }"###;
 
-    assert_eq!(enpoint_debug, format!("{:#?}", endpoint))
+    assert_eq!(endpoint_debug, format!("{:#?}", endpoint))
     }
-
-
-    // struct GitlabWithMockProject {
-    //     project: Result<Project>,
-    // }
-
-    // impl IfGitLabCreateProject for GitlabWithMockProject {
-    //     fn create_project<N: AsRef<str>, P: AsRef<str>>(
-    //         &self,
-    //         _name: N,
-    //         _path: Option<P>,
-    //         _params: Option<CreateProjectParams>,
-    //     ) -> Result<Project> {
-    //         match &self.project {
-    //             Ok(p) => Ok(p.clone()),
-    //             Err(e) => Err(anyhow!("{}", e)),
-    //         }
-    //     }
-    // }
-
-    // fn load_mock_from_disk<P: AsRef<Path>, T>(path: P) -> T
-    // where
-    //     T: DeserializeOwned,
-    // {
-    //     // Open the file in read-only mode with buffer.
-    //     let file = File::open(path).unwrap();
-    //     let reader = BufReader::new(file);
-
-    //     // Read the JSON contents of the file as an instance of `Project`.
-    //     serde_json::from_reader(reader).unwrap()
-    // }
-
-    // #[test]
-    // fn test_create_basic_project() {
-    //     // GIVEN:
-    //     let p_cmd = project::Project{
-    //         clap_cmd: ClapSubCommand::with_name("project")
-    //     };
-    //     let args = p_cmd
-    //         .gen_clap_command()
-    //         .get_matches_from(vec!["project", "create", "project_name"]);
-    //     let matches = args.subcommand_matches("create");
-
-    //     let mock_project: Project = load_mock_from_disk("tests/data/project.json");
-
-    //     let g = GitlabWithMockProject {
-    //         project: Ok(mock_project),
-    //     };
-
-    //     // WHEN:
-    //     let p = create_project_cmd(matches.unwrap().clone(), g);
-
-    //     // THEN:
-    //     assert!(p.is_ok())
-    // }
 }
