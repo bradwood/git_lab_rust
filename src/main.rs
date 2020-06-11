acaac191c57d0bb280dff0060afa5542f001144a
Bradley Wood <git@bradleywood.com>
2020-06-11 23:31:35 +0100

    bump version and update README.md

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 //! [![pipeline status](https://gitlab.com/bradwood/git-lab-rust/badges/master/pipeline.svg)](https://gitlab.com/bradwood/git-lab-rust/-/commits/master)
 //! [![coverage report](https://gitlab.com/bradwood/git-lab-rust/badges/master/coverage.svg)](https://gitlab.com/bradwood/git-lab-rust/-/commits/master)
 //!
-//! __WORK IN PROGRESS__ use at your own risk!
+//! _ALPHA_ - what is here works, but functionality is still under active development.
 //!
 //! This is a cli tool that adds the `lab` command to `git` to enable interaction with a GitLab server.
 //!
-//! # Features
+//! # Functionality
 //!
-//! The tool is designed to work as a custom command to the vanilla `git` cli command. Current
-//! functions include:
+//! The tool is designed to work as a custom command to the vanilla `git` cli command.
+//!
+//! ## Current functions
+//!
 //! * `init` -- initialise credentials aganst a remote GitLab server
 //! * `project` -- interact with GitLab projects
 //!     * `project create` -- create project
 //!     * `project attach` -- associate a local repo with a project
+//!     * `project (open|view|browse)` -- open project's URL in browser
 //!
-//! It has the following features:
-//! * config stored using standard `git config` machinery
+//! ## Planned functions
+//!
+//! * `issue` -- interact with issues
+//! * `merge-request` -- interact with merge requests
+//! * `pipeline` -- interact with Gitlab CI jobs
+//! * probably others
+//!
+//! # Features
+//!
+//! ## Current features
+//!
+//! * Config stored using standard `git config` machinery
 //! * JSON output in addition to plain text to allow for parsing with tools like `jq`.
+//!
+//! ## Planned features
+//!
+//! * `$EDITOR` integration
+//! * Terminal-based markdown rendering
 //!
 //! # Installation
 //!
@@ -25,6 +43,9 @@
 //! ```
 //! cargo install git_lab_cli
 //! ```
+//! # Compatibility
+//!
+//! Supports GitLab server version 13
 //!
 //! # Contributions
 //!
