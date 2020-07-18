1d583bb5be1972128e2ea28f39bc47cd2183a008
Bradley Wood <git@bradleywood.com>
2020-07-18 15:16:33 +0100

    docs: update README

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,20 @@
 //!  * `mr` -- interact with merge requests
 //!     * `mr create` -- create merge request (either entirely via cli-passed parameters, or
 //!        interactively, by prompting the user for the inputs needed)
+//!     * `mr close` -- close merge request
+//!     * `mr reopen` -- reopen merge request
+//!     * `mr lock` -- lock discussions on merge request
+//!     * `mr unlock` -- unlock discussions on merge request
 //!
 //! ## Planned functions
 //!
 //!  * `mr` -- interact with merge requests
+//!     * `mr list` -- get list of merge requests
 //!     * `mr (open|view|browse)` -- open merge request's URL in browser
 //!     * `mr (show|info|get)` -- show details about a merge request
-//!     * `mr list` -- get list of merge requests
-//!     * `mr close` -- close merge request
 //!     * `mr approve` -- approve merge request
 //!     * `mr merge` -- merge merge request
-//!     * `mr lock` -- lock discussions on merge request
-//!     * `mr unlock` -- unlock discussions on merge request
+//!     * `mr (checkout|co)` -- checkout merge request
 //!  * `project list` -- get list of projects
 //!  * `pipeline` -- interact with Gitlab CI jobs
 //!  * `group` -- interact with Gitlab groups
