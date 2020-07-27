69584342acb97108047f78aaa99f50b4a89d4543
Bradley Wood <git@bradleywood.com>
2020-07-27 18:30:00 +0100

    docs: add more details to README.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,16 @@
 //! [![Crates.io](https://img.shields.io/crates/d/git_lab_cli)](https://crates.io/crates/git_lab_cli)
 //! [![musl binaries](https://img.shields.io/badge/musl%20binary-download-brightgreen)](https://gitlab.com/bradwood/git-lab-rust/-/releases)
 //!
-//! _ALPHA_ - what is here works, but functionality is still under active development.
-//!
 //! This is a cli tool that adds the `lab` command to `git` to enable interaction with a GitLab server.
-//!
-//! # Functionality
-//!
-//! The tool is designed to work as a custom command to the vanilla `git` cli command.
 //!
 //! ## Change Log
 //!
 //! See [CHANGELOG.md](CHANGELOG.md) for a summary of fixes and features added to each release.
+//!
+//! # Functionality
+//!
+//! The tool is designed to work as a custom command to the vanilla `git` cli command. Once
+//! installed you can invoke it with `git lab <subcommand> <options>...`.
 //!
 //! ## Current functions
 //!
@@ -75,28 +74,67 @@
 //! ## Planned features
 //!
 //!  * support for various linux packaging tools like AUR, Deb, RPM, etc
-//!  * non-Linux support (maybe)
+//!  * non-Linux support (PRs requested!)
 //!
 //! # Installation
 //!
 //! ## Cargo
 //!
-//! To install via `cargo`:
+//! If you have the Rust toolchain installed, you can install via `cargo`:
 //!
-//! ```
+//! ```shell
 //! cargo install git_lab_cli
 //! ```
 //! ## Statically linked Linux binaries
 //!
 //! Grab a tarball for these [here](https://gitlab.com/bradwood/git-lab-rust/-/releases).
+//! Untar the file and then copy the included files into place as shown in the below example:
+//!
+//! ```shell
+//! cd git_lab_cli-x.y.z-x86_64-unknown-linux-musl
+//! sudo cp git-lab /usr/local/bin/
+//! sudo cp man/git-lab.1 /usr/local/share/man/man1/
+//! ```
+//!
+//! # Getting started
+//!
+//! To connect the `git-lab` cli binary to a GitLab instance you need to create a Personal Access
+//! Token. On Gitlab.com this can be done
+//! [here](https://gitlab.com/profile/personal_access_tokens). Copy the token to your clipboard and
+//! then run the following from your home directory:
+//!
+//! ```shell
+//! git lab init
+//! ```
+//!
+//! This will prompt you though entering the required set-up parameters, one of which will require
+//! you to paste the GitLab token copied in the previous step into the config. Your default
+//! `.gitconfig` will then be updated with the information needed to connect `git-lab` to your
+//! server. You can also set this config up with vanilla `git config` commands. See `git lab init
+//! --help` for details on how to do this.
+//! 
+//! The easiest way to get started with an existing git repo is to run the following from _within_
+//! the repo:
+//!
+//! ```shell
+//! git lab project attach
+//! ```
+//!
+//! This will assoicate the git repo you are working on with a server-side GitLab project by
+//! looking at your `origin` git remote. Once this is done, you'll be able to query, create, and
+//! modify project-specific objects like GitLab issues, merge-requests, and such like as long as
+//! you remain within the repo's subtree.
 //!
 //! # Compatibility
 //!
-//! Supports GitLab server version 13
+//! The tool tries to track GitLab.com's latest version pretty closely. Currently GitLab 13.0 and
+//! above work but earlier versions do not.
 //!
 //! # Contributions
 //!
-//! Merge requests are welcome. Please raise a merge request on [GitLab](https://gitlab.com/bradwood/git-lab-rust), not GitHub.
+//! Merge requests are welcome. Please raise a merge request on
+//! [GitLab](https://gitlab.com/bradwood/git-lab-rust), not GitHub.
+//!
 
 #[macro_use]
 extern crate log;
